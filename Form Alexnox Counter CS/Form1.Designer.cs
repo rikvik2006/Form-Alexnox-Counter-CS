931a75ba98f2ec6798f83f95bc5f42f20ad2e300
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2021-08-26 22:25:29 +0200

    Initial commit

---
+++ Form Alexnox Counter CS/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Form_Alexnox_Counter_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Count = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Count
+            // 
+            this.Count.AutoSize = true;
+            this.Count.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Count.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(2)))), ((int)(((byte)(25)))));
+            this.Count.Location = new System.Drawing.Point(194, 155);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(98, 108);
+            this.Count.TabIndex = 0;
+            this.Count.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "+1 Manuale";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-1 Manuale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button2_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button3_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(393, 25);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(59, 33);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "HELP";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Count);
+            this.Name = "Form1";
+            this.Text = "DeatCounter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Count;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form Alexnox Counter CS/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Form_Alexnox_Counter_CS.Properties;
+
+
+//Count
+
+
+namespace Form_Alexnox_Counter_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Count.Text = counter.ToString();
+        }
+
+        
+        int counter = (int)Settings.Default["Count"];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter++;
+            Count.Text = counter.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counter--;
+            Count.Text = counter.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Settings.Default["Count"] = int.Parse(Count.Text);
+            Settings.Default.Save();
+            MessageBox.Show("Salvato");
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control == true && e.KeyCode == Keys.F17)
+            {
+                counter++;
+                Count.Text = counter.ToString();
+            }
+
+        }
+
+        private void button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control == true && e.KeyCode == Keys.F18)
+            {
+                counter--;
+                Count.Text = counter.ToString();
+            }
+        }
+
+        private void button3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control == true && e.KeyCode == Keys.F19)
+            {
+                Settings.Default["Count"] = int.Parse(Count.Text);
+                Settings.Default.Save();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("+1 = Ctrl + F17 \n\n -1 = Ctrl + F18 \n\n Save = Ctrl + F19");
+        }
+    }
+}
